c8128c7bd9ed8ef37477264b171dcb4caa17b8d8
pedrazasofia49-cyber <pedrazasofia49@gmail.com>
2025-09-01 16:22:43 -0500

    Homework 1

---
+++ Homework 1.cs
@@ -0,0 +1,13 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double X = 2.5;
+        double Y = 3.3;
+        double Z = 4 * X * X + 3 * Y;
+
+        Console.WriteLine($"When X = {X} and Y = {Y}, the value of Z = {Z}");
+    }
+}
