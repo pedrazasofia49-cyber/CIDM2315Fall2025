0673c3ee518436b55b3ac1c56be2e156fafe7307
pedrazasofia49-cyber <pedrazasofia49@gmail.com>
2025-09-29 21:36:46 -0500

    Homework 5- Q1

---
+++ Homework 5.cs
@@ -0,0 +1,22 @@
+﻿namespace Homework5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int largest = GetLargestNumber();
+        Console.WriteLine($"The largest number is: {largest}");
+    }
+
+    static int GetLargestNumber()
+    {
+        Console.Write("Enter the first integer: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the second integer: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        return (num1 > num2) ? num1 : num2;
+    
+    }
+}
