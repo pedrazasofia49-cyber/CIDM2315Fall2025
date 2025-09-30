d8261e4335e10f806c3679733e04561f8bfd78fa
pedrazasofia49-cyber <pedrazasofia49@gmail.com>
2025-09-29 21:38:26 -0500

    Homework 5- Q3

---
+++ Homework 5.3.cs
@@ -0,0 +1,54 @@
+﻿namespace Homework5._3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+         createAccount();
+    }
+
+    
+    static bool checkAge(int birth_year)
+    {
+        int currentYear = DateTime.Now.Year;
+        int age = currentYear - birth_year;
+
+        if (age >= 18)
+            return true;
+        else
+            return false;
+    }
+
+    
+    static void createAccount()
+    {
+        Console.Write("Enter username: ");
+        string username = Console.ReadLine();
+
+        Console.Write("Enter password: ");
+        string password1 = Console.ReadLine();
+
+        Console.Write("Re-enter password: ");
+        string password2 = Console.ReadLine();
+
+        Console.Write("Enter birth year: ");
+        int birthYear = int.Parse(Console.ReadLine());
+
+        if (checkAge(birthYear))
+        {
+            if (password1 == password2)
+            {
+                Console.WriteLine("Account is created successfully");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Could not create an account");
+        }
+    
+    }
+}
